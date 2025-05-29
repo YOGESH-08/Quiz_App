5989ef496c180fb7d55387085c3886ed318fb74c
Yogesh Kumar <yogeshkumar.n1234@gmail.com>
2025-05-29 15:22:17 +0530

    check_success

--- server/index.js
+++ server/index.js
@@ -1,9 +1,24 @@
 import express from "express";
 import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const corsOption = {
+  origin: ["http://localhost:5173"],
+};
 
 const app = express();
-app.use(bodyParser.urlencoded({extended:true}));
 
-app.get("/api",(req,res)=>{
-    
-})
+app.use(bodyParser.urlencoded({ extended: true }));
+const port = process.env.PORT;
+app.use(cors(corsOption));
+
+app.get("/api", (req, res) => {
+  res.json({ fruits: ["apple", "orange", "bananna"] });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}/api`);
+});
